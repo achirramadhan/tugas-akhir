afa28e6b64cc93348acfb93961849f14f9ea8a2f
Muhamad Achir Suci Ramadhan <muhamadachir99@gmail.com>
2020-12-03 10:24:11 +0000

    Add holdout and shell adult dataset

---
+++ experiment/adult/imli/start-1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+HOME="/root"
+DIR_RUNNER="$HOME/skripsi/imli/experiment"
+DIR_RESULT="$HOME/skripsi/imli/experiment/adult/imli"
+
+python $DIR_RUNNER/imli_runner_methodology_validation.py adult 1 1 3 5 6 7 8 13 >$DIR_RESULT/result-imli-adult-1.txt 2>&1
