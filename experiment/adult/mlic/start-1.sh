b95f566fafe35a7d385a261529da6de76220474f
Muhamad Achir Suci Ramadhan <muhamadachir99@gmail.com>
2020-12-09 09:57:25 +0000

    Add adult result imli and mlic

---
+++ experiment/adult/mlic/start-1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+HOME="/root"
+DIR_RUNNER="$HOME/skripsi/imli/experiment"
+DIR_RESULT="$HOME/skripsi/imli/experiment/adult/mlic"
+
+python $DIR_RUNNER/mlic_runner_methodology_validation.py adult 1 1 3 5 6 7 8 13 >$DIR_RESULT/result-mlic-adult-1.txt 2>&1
