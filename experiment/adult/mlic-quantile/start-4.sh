0c96faea1ede96b42106c96c3530635995881516
Muhamad Achir Suci Ramadhan <muhamadachir99@gmail.com>
2020-12-26 06:29:56 +0000

    Checkpoint

---
+++ experiment/adult/mlic-quantile/start-4.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+HOME="/root"
+DIR_RUNNER="$HOME/skripsi/imli/experiment"
+DIR_RESULT="$HOME/skripsi/imli/experiment/adult/mlic-quantile"
+
+python $DIR_RUNNER/mlic_runner_methodology_validation_quantile.py adult 4 1 3 5 6 7 8 13 >$DIR_RESULT/result-mlic-quantile-adult-4.txt 2>&1
